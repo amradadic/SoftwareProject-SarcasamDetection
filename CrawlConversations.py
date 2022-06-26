8ae817e8ab7380aee4fd534009270225484c441b
ekarahodza1 <56482990+ekarahodza1@users.noreply.github.com>
2022-06-26 16:26:35 +0200

    functions for crawling conversation (begining of this)

---
+++ CrawlConversations.py
@@ -0,0 +1,44 @@
+import requests
+import os
+import json
+
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAAI3qcQEAAAAA4PAEfL%2ByQCQVQJpHQdNdCoBtlS0%3DvNGdgosAW0lrgv5t8alZ2buQSRfdIYVNT4KCw1Xyvql09qulAS'
+
+def bearer_oauth(r):
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2RecentSearchPython"
+    return r
+
+
+def connect_to_endpoint(url, params):
+    response = requests.get(url, auth=bearer_oauth, params =params)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
+
+def getConversationId(tweet_id):
+    search_url = "https://api.twitter.com/2/tweets"
+
+    expansions = 'author_id,in_reply_to_user_id,referenced_tweets.id'
+    fields = 'author_id,conversation_id,created_at,in_reply_to_user_id,referenced_tweets'
+    userfields = 'name,username'
+    query_params = {'ids' : tweet_id, 'tweet.fields' :fields, 'expansions' : expansions, 'user.fields': userfields}
+    json_response = connect_to_endpoint(search_url, query_params)
+    print(json.dumps(json_response, indent=4, sort_keys=True))
+
+    #this should return conversation_id
+
+def getConversationTweets(conversation_id):
+    search_url = "https://api.twitter.com/2/tweets/search/recent"
+    expansions = 'referenced_tweets.id,in_reply_to_user_id'
+    fields = 'in_reply_to_user_id,author_id,created_at,conversation_id'
+
+    query_params = {'query': conversation_id, 'tweet.fields': fields, 'expansions': expansions, 'max_results': 100}
+    json_response = connect_to_endpoint(search_url, query_params)
+    print(json.dumps(json_response, indent=4, sort_keys=True))
+    #this should filter the json, remove the rt and other extras and return the conversation as a list
+
+
+getConversationId(1540746631515103232)
+getConversationTweets(1540746631515103232)
--- twitterAPI.py
+++ twitterAPI.py
@@ -6,27 +6,47 @@ import json
 # export 'BEARER_TOKEN'='<your_bearer_token>'
 bearer_token = 'AAAAAAAAAAAAAAAAAAAAAI3qcQEAAAAA4PAEfL%2ByQCQVQJpHQdNdCoBtlS0%3DvNGdgosAW0lrgv5t8alZ2buQSRfdIYVNT4KCw1Xyvql09qulAS'
 
-#search_url = "https://api.twitter.com/2/tweets/search/recent"
-search_url = "https://api.twitter.com/2/tweets"
+
+#curl --request GET \
+#  --url 'https://api.twitter.com/2/tweets/search/recent?query=conversation_id:1279940000004973111&
+#  tweet.fields=in_reply_to_user_id,author_id,created_at,conversation_id' \
+#  --header 'Authorization: Bearer $BEARER_TOKEN'
+
+search_url = "https://api.twitter.com/2/tweets/search/recent"
+#search_url = "https://api.twitter.com/2/tweets"
 # Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
 # expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
-ids = '1225917697675886593'
-expansions = 'author_id,in_reply_to_user_id,referenced_tweets.id'
-fields = 'author_id,conversation_id,created_at,in_reply_to_user_id,referenced_tweets'
-userfields = 'name,username'
-query_params = {'ids' : ids, 'tweet.fields' :fields, 'expansions' : expansions, 'user.fields': userfields}
-def bearer_oauth(r):
-    """
-    Method required by bearer token authentication.
-    """
 
+
+#pokusaj amra
+ids = '1540746631515103232'
+expansions = 'author_id,in_reply_to_user_id,referenced_tweets.id'
+#fields = 'author_id,conversation_id,created_at,in_reply_to_user_id,referenced_tweets'
+
+userfields = 'name,username'
+#query_params = {'ids' : ids, 'tweet.fields' :fields, 'expansions' : expansions, 'user.fields': userfields}
+
+#pokusaj esma
+expansions = 'referenced_tweets.id,in_reply_to_user_id'
+fields = 'in_reply_to_user_id,author_id,created_at,conversation_id'
+
+query_params = {'query': ids, 'tweet.fields' :fields, 'expansions' : expansions, 'max_results': 100}
+
+#
+#userfields = 'name,username'
+#query_params = {'ids' : ids, 'tweet.fields' :fields, 'expansions' : expansions, 'user.fields': userfields}
+
+
+
+
+
+
+
+
+def bearer_oauth(r):
     r.headers["Authorization"] = f"Bearer {bearer_token}"
     r.headers["User-Agent"] = "v2RecentSearchPython"
     return r
-
-
-
-
 
 
 def connect_to_endpoint(url, params):
