76374eff12d10c4de30592bcb1c59b68b3ecffd2
ekarahodza1 <56482990+ekarahodza1@users.noreply.github.com>
2022-07-04 20:12:25 +0200

    recognizing sarcastic, oblivious and elicit tweets from pattern

--- CrawlConversations.py
+++ CrawlConversations.py
@@ -1,3 +1,4 @@
+import re
 import requests
 import pandas as pd
 import time
@@ -12,13 +13,13 @@ def bearer_oauth(r):
     return r
 
 def connect_to_endpoint(url, params):
-    response = requests.get(url, auth=bearer_oauth, params =params)
-    #print(response.status_code)
-    if response.status_code == 429:
-        timeout = 900 #15min
+    response = requests.get(url, auth=bearer_oauth, params=params)
+
+    while response.status_code == 429:
+        timeout = 100  #15min
         print(f'Sleeping for {timeout} seconds')
         time.sleep(timeout)
-        connect_to_endpoint(url, params)
+        response = requests.get(url, auth=bearer_oauth, params=params)
     if response.status_code != 200:
         raise Exception(response.status_code, response.text)
     return response.json()
@@ -60,12 +61,58 @@ def getThreadRecursively(tweet_id):
 def next_alpha(s):
     return chr((ord(s.upper())+1 - 65) % 26 + 65)
 
+
+def recognizeTypeOfTweet(thread_pattern, list):
+
+    index_list = []
+    if len(list) == 0:
+        return index_list
+
+    tweet_text =  list[0][1]
+    lice = 0
+    if re.match("(A)([^A]*)(A)([^A]*)$", thread_pattern) and (('I was' in tweet_text) or ('I am' in tweet_text) or ('I\'m' in tweet_text)):
+
+        index_of_A = ([pos for pos, char in enumerate(thread_pattern) if char == 'A'])
+
+        #cue, oblivious, sarcastic, elicit
+        index_list = [0, 1, index_of_A[1], index_of_A[1] + 1]
+
+    elif re.match("(A)A*(B)(A*)$", thread_pattern) and  (('you\'re') in tweet_text
+                                                         or ('you are') in tweet_text
+                                                         or ('are you') in tweet_text
+                                                         or ('you were') in tweet_text
+                                                         or ('were you') in tweet_text):
+
+        index_of_B = ([pos for pos, char in enumerate(thread_pattern) if char == 'B'])
+
+        # cue, oblivious, sarcastic, elicit
+        index_list = [0, -1, index_of_B[0], index_of_B[0] + 1]
+
+    elif re.match("(A)(A*B[AB]*)(C)([AB]*)$", thread_pattern) and (('she is') in tweet_text
+                                                                   or ('she was') in tweet_text
+                                                                   or ('was she') in tweet_text
+                                                                   or ('she\'s') in tweet_text
+                                                                   or ('he is') in tweet_text
+                                                                   or ('he was') in tweet_text
+                                                                   or ('was he') in tweet_text
+                                                                   or ('he\'s') in tweet_text
+                                                                   or ('they were') in tweet_text
+                                                                   or ('they\'re') in tweet_text
+                                                                   or ('were they') in tweet_text
+                                                                   or ('they are') in tweet_text):
+
+        index_of_C = ([pos for pos, char in enumerate(thread_pattern) if char == 'C'])
+        # cue, oblivious, sarcastic, elicit
+        index_list = [0, 1, index_of_C[0], index_of_C[0] + 1]
+
+    return index_list
+
 def defineExpressionsForTweet():
     df = pd.read_csv('filtered.csv')
-    for i in range(110, df.shape[0]):
+    for i in range(204, 210):  #df.shape[0]
         replies_list = getThreadRecursively(df.at[i,'tweet_id'])
         s = ''
-        replies_list.reverse()
+        #replies_list.reverse()
         author_id_list = {}
         letter = 'A'
         for x in replies_list:
@@ -76,6 +123,12 @@ def defineExpressionsForTweet():
                 letter = next_alpha(letter)
             else:
                 s += author_id_list[current_id]
+
+        index_list_types = recognizeTypeOfTweet(s, replies_list)
+
+        print(index_list_types, s)
+
+
 
         #print(s, " ", df.at[i,'tweet_id'])
         df.loc[i, 'pattern'] = s
